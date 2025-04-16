2bf8889feacb5c9129b15a2edd28b7e3acca025e
rahulrameshm0 <iamrahulm23@gmail.com>
2025-04-17 01:20:33 +0530

    added product list template

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('products/', views.product_list, name='product_list')
+    path('', views.index, name='home'),
+    path('products/', views.product_list, name='product_list'),
 ]
