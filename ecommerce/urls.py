3924462be0ffbb5eb09b97063a828d85d501ae78
rahulrameshm0 <iamrahulm23@gmail.com>
2025-04-18 21:53:00 +0530

    created account and cart templates

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('products.urls')),
+    path('customers/', include('customers.urls')),
+    path('orders/', include('orders.urls'))
 ]
-
