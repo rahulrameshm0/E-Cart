fc5f1b798f045ac6339472d2cc5a00e927e63dc3
rahulrameshm0 <iamrahulm23@gmail.com>
2025-04-20 17:27:31 +0530

    created admin section

--- products/views.py
+++ products/views.py
@@ -1,14 +1,16 @@
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from django.contrib.auth.models import User
-
+from django . shortcuts import render, redirect
+from django . contrib import messages
+from django . contrib.auth.models import User
+from . models import Product
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
 
 def product_list(request):
-    return render(request, 'products.html')
+    products_list = Product.objects.all()
+    context = {'products': products_list}
+    return render(request, 'products.html', context)
 
 def product_details(request):
     return render(request, 'product_details.html')
