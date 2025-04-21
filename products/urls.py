b367ce5fc4331fa746268b05d154317c25abef69
rahulrameshm0 <iamrahulm23@gmail.com>
2025-04-21 20:48:16 +0530

    implimented featured and latest features in the home section

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('products/', views.product_list, name='product_list'),
-    path('product_details/', views.product_details,name='product_details'),
+    path('product_details/<pk>', views.product_details,name='product_details'),
 ]
--- products/views.py
+++ products/views.py
@@ -6,18 +6,26 @@ from django.core.paginator import Paginator
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    featured_products = Product.objects.order_by('priority')[:4]
+    latest_products = Product.objects.order_by('-id')[:4]
+    context = {
+       'featured_products':featured_products,
+       'latest_products':latest_products 
+    }
+    return render(request, 'index.html', context)
 
-def product_list(request):
+def product_list(request): 
 
     page = 1
     if request.GET:
         page = request.GET.get('page', 1)
-    products_list = Product.objects.all()
-    product_paginator = Paginator(products_list, 12)
+    products_list = Product.objects.order_by('priority')
+    product_paginator = Paginator(products_list, 2)
     product_list = product_paginator.get_page(page) 
     context = {'products': product_list}
     return render(request, 'products.html', context)
 
-def product_details(request):
-    return render(request, 'product_details.html')
+def product_details(request, pk):
+    product = Product.objects.get(pk=pk)
+    context = {'product':product}
+    return render(request, 'product_details.html', context)
